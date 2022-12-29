e7daa29ef27da9f465794bbf3e3eae14720cac9d
unknown <msandoval@tolkogroup.com>
2022-12-29 10:45:41 +0100

    NEW FEATURE: Se crea Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import Sidebar from './components/sidebar';
 import Footer from './components/footer';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Sidebar/>
       <Footer/>
     </div>
